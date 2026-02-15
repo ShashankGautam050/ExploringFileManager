cdaa67203ba671dfabb50ff0fb14664b8f0cdd1d
Shashank Gautam <shashank320722@gmail.com>
2026-02-15 09:49:04 +0530

    made instance of file manager and created a file

--- ExploringFileManager/ViewController.swift
+++ ExploringFileManager/ViewController.swift
@@ -11,8 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // shared instance of file manager
+        let manager = FileManager.default
+        
+        // url of the document directory
+        guard let url = manager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return
+        }
+        
+        
+        print(url.path)
+        
+        let createFolder = url.appendingPathComponent("shashank")
+        let filePath = createFolder.appending(path: "file1.txt")
+        let string = "hello world".data(using: .utf8)
+        manager.createFile(atPath: filePath.path, contents: string, attributes: [:])
+        
+        
     }
+    
 
 
 }
